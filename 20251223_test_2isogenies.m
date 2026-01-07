3f08623ea73c2410febfefbda3dc3d26774c8510
stmar89 <stefano.marseglia89@gmail.com>
2026-01-07 15:15:42 +0100

    comments in paper

--- 20251223_test_2isogenies.m
+++ 20251223_test_2isogenies.m
@@ -10,8 +10,8 @@
 
     SetColumns(0);
     PP<x>:=PolynomialRing(Integers());
-    h:=1 - 3*x + 5*x^2 - 9*x^3 + 9*x^4;
-    //h:=1 - 7*x + 31*x^2;
+    h:=1 - 3*x + 5*x^2 - 9*x^3 + 9*x^4; // 2.3.ad_f
+    //h:=1 - 7*x + 31*x^2; // 1.31.ah
     h:=PP!Reverse(Coefficients(h));
     K:=EtaleAlgebra(h);
     F:=PrimitiveElement(K);
@@ -23,4 +23,43 @@
         Append(~Ris,Ri);
         printf "i=%o\tnorms_Ps_above 2=%o\t[R1:Ri]=%o\n",i, [ Index(Ri,P) : P in PrimesAbove(2*Ri) ],pretty_fac_Z(Index(Ris[1],Ri)); 
     end for;
+
+    SetColumns(0);
+    PP<x>:=PolynomialRing(Integers());
+    h:=1+2*x-x^2-5*x^3-8*x^4-15*x^5-9*x^6+54*x^7+81*x^8;
+    h:=PP!Reverse(Coefficients(h));
+    K:=EtaleAlgebra(h);
+    F:=PrimitiveElement(K);
+    q:=Round(ConstantCoefficient(h)^(2/Degree(h)));
+    _,p:=IsPrimePower(q);
+    V:=q/F;
+    Ris:=[];
+    for i in [1..30] do 
+        Ri:=Order([F^i,V^i]); 
+        Index(Ri,Order([F^i])) mod p;
+//        Append(~Ris,Ri);
+//        printf "i=%o\tnorms_Ps_above 2=%o\t[R1:Ri]=%o\n",i, [ Index(Ri,P) : P in PrimesAbove(2*Ri) ],pretty_fac_Z(Index(Ris[1],Ri)); 
+    end for;
+
+    IsMaximal(Ris[1]);
+
+// looking for Frob orders R c K such that [OK:R] is divisible by 2, but R has no ideals of index 2.
+    all:=Split(Read("weil_poly_sqfree_ord.txt"));
+    PP<x>:=PolynomialRing(Integers());
+    for c in all do
+        h:=PP!eval(c);
+        K:=EtaleAlgebra(h);
+        F:=PrimitiveElement(K);
+        q:=Round(ConstantCoefficient(h)^(2/Degree(h)));
+        _,p:=IsPrimePower(q);
+        V:=q/F;
+        OK:=MaximalOrder(K);
+        R:=Order([F,V]);
+        if Index(OK,R) mod 2 eq 0 and #IdealsOfIndex(1*R,2) eq 0 then
+            h;
+        end if;
+    end for;
+
+
+
 
