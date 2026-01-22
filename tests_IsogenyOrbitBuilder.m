d123a13687b81e118c9395240ac0d0e2853c03de
stmar89 <stefano.marseglia89@gmail.com>
2026-01-22 15:01:25 +0100

    streamlining tests for Orbit builder; bug found. see tests_ISogenyOrbitBuilder.m

---
+++ tests_IsogenyOrbitBuilder.m
@@ -0,0 +1,30 @@
+/*
+    Tests for IsogenyOrbitBuilder
+*/
+
+    AttachSpec("~/AbVarFq_Isogenies_Private/magma/spec");
+    _<x>:=PolynomialRing(Integers());
+    f:=x^4-2*x^2+121;
+    q:=Round(ConstantCoefficient(f)^(2/Degree(f)));
+    K:=EtaleAlgebra(f);
+    F:=PrimitiveElement(K);
+    V:=q/F;
+    R:=Order([F,V]);
+
+    for D in [2,3,4,6,8,9] do
+        printf "D=%o\t",D;
+        mismatches:=IsogenyGraphChecker(R,D); 
+        if #mismatches ne 0 then
+            printf "...got issues\n";
+        else
+            printf "...all good\n";
+        end if;
+    end for;
+
+    // with Profiler
+    SetProfile(true);
+    _:=IsogenyOrbitBuilder(R,6);
+    SetProfile(false);
+    G:=ProfileGraph();
+    ProfilePrintByTotalTime(G:Max:=10);
+
