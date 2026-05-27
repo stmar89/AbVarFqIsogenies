bb0b8375128fe925d3bcb969a382dfb5f9727da5
David Roe <roed.math@gmail.com>
2026-05-27 08:55:13 -0700

    Some fixes found by LLM

--- OrbitBuilder.m
+++ OrbitBuilder.m
@@ -268,24 +268,26 @@ intrinsic IsogenyOrbitBuilder(R::AlgEtQOrd, D::RngIntElt : dual_only:=false) -> 
             if d2 lt n and n mod d2 eq 0 then
                 d1 := n div d2;
                 for E2 in E_min_d2 do
-                    E2_s := E2[1][1];
+                    E2_s := E2[1][1]; E2_t := E2[2][1]
                     if not IsDefined(reps[d1], E2_s) then reps[d1][E2_s] := AssociativeArray(); end if;
                     // now, we loop over all already computed reps E1 of degree d1=n/d2 such
                     // that target(E1) = source(E2) = E2[1][1], since we want to construct the composition E1*E2
                     // (first apply the isogeny E1 then the isogeny E2)
-                    for E1_t->E1s in reps[d1][E2_s] do
-                        if n eq D and dual_only and E1_t ne dual_we[E2_s] then
+                    for E1_s->E1s in reps[d1][E2_s] do
+                        if n eq D and dual_only and E1_s ne dual_we[E2_t] then
                             // We don't need degree D isogenies for further composition, so skip if not needed for output.
                             continue;
                         end if;
                         for E1 in E1s do
-                            if not IsDefined(reps[n], E2[2][1]) then reps[n][E2[2][1]] := AssociativeArray(); end if;
-                            if not IsDefined(reps[n][E2[2][1]],E1[1][1]) then reps[n][E2[2][1]][E1[1][1]]:=[]; end if;
+                            E1_s := E1[1][1]; E1_t := E1[2][1]
+                            if not IsDefined(reps[n], E2_t) then reps[n][E2_t] := AssociativeArray(); end if;
+                            if not IsDefined(reps[n][E2_t],E1_s) then reps[n][E2_t][E1_s]:=[]; end if;
                             for Ecomp in GSTCompose(E1, E2) do
                                 //if not exists{E:E in reps[n][E2[2][1]][E1[1][1]]|
-                                if not IsDefined(reps[n], Ecomp[2][1]) then reps[n][Ecomp[2][1]] := AssociativeArray(); end if;
-                                if not IsDefined(reps[n][Ecomp[2][1]], Ecomp[1][1]) then reps[n][Ecomp[2][1]][Ecomp[1][1]] := []; end if;
-                                if not exists{E:E in reps[n][Ecomp[2][1]][Ecomp[1][1]]|
+                                Ecomp_s := Ecomp[1][1]; Ecomp_t := Ecomp[2][1];
+                                if not IsDefined(reps[n], Ecomp_t) then reps[n][Ecomp_t] := AssociativeArray(); end if;
+                                if not IsDefined(reps[n][Ecomp_t], Ecomp_s) then reps[n][Ecomp_t][Ecomp_s] := []; end if;
+                                if not exists{E:E in reps[n][Ecomp_t][Ecomp_s]|
                                     AreIsogeniesGSTEquivalent(Ecomp,E)
                                     } then
                                     // Asserts for debugging
@@ -294,7 +296,7 @@ intrinsic IsogenyOrbitBuilder(R::AlgEtQOrd, D::RngIntElt : dual_only:=false) -> 
                                     //    EE := reps[n][Ecomp[2][1]][Ecomp[1][1]][1];
                                     //    assert not AreIsogeniesEquivalent(Ecomp[5],Ecomp[3],Ecomp[4],EE[5],EE[3],EE[4]);
                                     //end if;
-                                    Append(~reps[n][Ecomp[2][1]][Ecomp[1][1]], Ecomp);
+                                    Append(~reps[n][Ecomp_t][Ecomp_s], Ecomp);
                                 end if;
                             end for;
                         end for;
